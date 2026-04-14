b9731fe0b4b112db24a15502a2f9f120568d7e48
khalaffarhan <khalaffarhan54@gmail.com>
2026-04-14 17:00:03 +0700

    Variable koneksi

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -9,3 +9,33 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        // Variabel koneksi - GANTI sesuai nama server SQL kamu
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=LAPTOP-P0RT1FO1;Initial Catalog=DBAkademikADO;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        // Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
