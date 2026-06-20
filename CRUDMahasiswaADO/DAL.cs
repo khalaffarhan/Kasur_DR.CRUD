40b5a25a2f7d8fdee56c3ecabaaff78d551e9029
khalaffarhan <khalaffarhan54@gmail.com>
2026-06-20 20:19:13 +0700

    Menambahkan method testInject untuk mendemonstrasikan celah keamanan pada query. Menggunakan teknik penggabungan string (string concatenation) pada parameter nim untuk keperluan pengujian

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -178,3 +178,15 @@ namespace CRUDMahasiswaADO
             SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
             cmdInsert.ExecuteNonQuery();
         }
+
+        public void testInject(string nim)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            string query = "Update mahasiswa set nama = 'HACKED' where NIM = '" + nim + "';";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
