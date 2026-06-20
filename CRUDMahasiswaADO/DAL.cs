8657e4a4fcfab70ce465c05af52a58d810c72a8e
khalaffarhan <khalaffarhan54@gmail.com>
2026-06-20 20:24:41 +0700

    Membuat fungsi getAllDataChart guna menarik dataset utama. Memanggil stored procedure sp_DashBoard.

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -258,3 +258,18 @@ namespace CRUDMahasiswaADO
             da.Fill(dtMahasiswa);
             return dtMahasiswa;
         }
+
+        public DataTable getAllDataChart()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_DashBoard", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(cmd);
+            dtMahasiswa = new DataTable();
+            da.Fill(dtMahasiswa);
+            return dtMahasiswa;
+        }
